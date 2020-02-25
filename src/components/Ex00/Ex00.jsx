940698706e1be96624252b3c7a61010a83baf283
Théo Boutron <contact@tboutron.pro>
2020-02-25 11:03:29 +0100

    Clean init code

--- src/components/Ex00/Ex00.jsx
+++ src/components/Ex00/Ex00.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 
 function Ex00() {
-  const [click, setClick] = useState(0);
-
   return (
     <div className="ex00">
       <h1>Ex00 - Etat d'un composant</h1>
 
-      <button onClick={() => setClick(click + 1)}>Compter</button>
+      <button>Compter</button>
       <p>Il y a eu {click} clic(s)</p>
     </div>
   );
